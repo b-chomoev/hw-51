c651f803ca0d49a9a9fdcbc3f3e48dc84a6a311b
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-09-12 01:28:57 -0500

    Completed task.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,13 @@ const App = () => {
     const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
 
     const randomNumbers = () => {
-        console.log('Hello');
+        setNumbers([
+            Math.floor(Math.random() * (10 - 5) + 5),
+            Math.floor(Math.random() * (15 - 11) + 11),
+            Math.floor(Math.random() * (22 - 16) + 16),
+            Math.floor(Math.random() * (31 - 23) + 23),
+            Math.floor(Math.random() * (36 - 32) + 32)
+        ]);
     };
 
     return (
