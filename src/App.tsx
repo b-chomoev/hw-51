88f20b81cdf4cc80d36344c038f4b71dd3ed3ef8
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-09-12 00:28:37 -0500

    Создал шарики в виде компонента и вывел их на экран с помощью map.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css';
+import LotteryBall from "./components/LotteryBall/LotteryBall";
+import {useState} from "react";
 
 const App = () => {
+    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+
+    const randomNumbers = () => {
+        console.log('Hello');
+    };
+
     return (
         <>
-
+            <div className='lottery-ball'>
+                {numbers.map((number, index) => (
+                    <LotteryBall key={index} number={number}/>
+                ))}
+            </div>
+            <button type='button' onClick={randomNumbers}>New numbers</button>
         </>
     );
 };
---
+++ src/components/LotteryBall/LotteryBall.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './LotteryBall.css';
+
+interface Props {
+    number: number;
+}
+
+const LotteryBall: React.FC<Props> = ({number}) => {
+    return (
+        <div className='lottery-number'>
+            <div>{number}</div>
+        </div>
+    );
+};
+
+export default LotteryBall;
