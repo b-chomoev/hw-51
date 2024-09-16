2340276f8e1a739e07fb8ccff4e551033acdda47
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-09-16 13:23:22 -0500

    Completed the logic of showing the numbers in array with more complex method.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import './App.css';
-import LotteryBall from "./components/LotteryBall/LotteryBall";
+import LotteryBall from "./Components/LotteryBall/LotteryBall";
 import {useState} from "react";
 
 const App = () => {
     const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
 
-    const randomNumbers = () => {
-        setNumbers([
-            Math.floor(Math.random() * (10 - 5) + 5),
-            Math.floor(Math.random() * (15 - 11) + 11),
-            Math.floor(Math.random() * (22 - 16) + 16),
-            Math.floor(Math.random() * (31 - 23) + 23),
-            Math.floor(Math.random() * (36 - 32) + 32)
-        ]);
+    const getRandomNumbers = () => {
+        let newNumberArray: number[] = [];
+
+        while (true) {
+            if (newNumberArray.length === 5) break;
+
+            let randomNumber = Math.floor(Math.random() * (36 + 1 - 5) + 5);
+
+            if (!newNumberArray.includes(randomNumber)) newNumberArray.push(randomNumber);
+        }
+
+        setNumbers(newNumberArray.sort((a, b) => a - b));
     };
 
     return (
@@ -22,7 +26,7 @@ const App = () => {
                     <LotteryBall key={index} number={number}/>
                 ))}
             </div>
-            <button type='button' onClick={randomNumbers}>New numbers</button>
+            <button type='button' onClick={getRandomNumbers}>New numbers</button>
         </>
     );
 };
