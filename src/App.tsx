0875e93fe4be7e43b46a7a8e0c985f008601c028
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-09-22 09:08:28 -0500

    Rewritten code by myself.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import './App.css';
 import LotteryBall from "./Components/LotteryBall/LotteryBall";
+import './App.css';
 import {useState} from "react";
 
 const App = () => {
     const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
 
     const getRandomNumbers = () => {
-        const newNumberArray: number[] = [];
+        const newNumbersArray: number[] = [];
 
         while (true) {
-            if (newNumberArray.length === 5) break;
+            if (newNumbersArray.length === 5) break;
 
             const randomNumber = Math.floor(Math.random() * (36 + 1 - 5) + 5);
 
-            if (!newNumberArray.includes(randomNumber)) newNumberArray.push(randomNumber);
-        }
+            if (!newNumbersArray.includes(randomNumber)) newNumbersArray.push(randomNumber);
 
-        setNumbers(newNumberArray.sort((a, b) => a - b));
+            setNumbers(newNumbersArray.sort((a, b) => a - b));
+        }
     };
 
     return (
         <>
             <div className='lottery-ball'>
-                {numbers.map((number, index) => (
-                    <LotteryBall key={index} number={number}/>
+                {numbers.map((number) => (
+                    <LotteryBall number={number}/>
                 ))}
             </div>
-            <button type='button' onClick={getRandomNumbers}>New numbers</button>
+            <button type='button' onClick={getRandomNumbers}>Change numbers</button>
         </>
     );
 };
--- src/Components/LotteryBall/LotteryBall.tsx
+++ src/Components/LotteryBall/LotteryBall.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import './LotteryBall.css';
 
 interface Props {
